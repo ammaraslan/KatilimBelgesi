828a2b4dc878edf8d885f2e4fd4e83348fb7c281
ASLAN <ammaraslan@gmail.com>
2022-02-09 19:28:40 +0300

    katilimci exceli oluşturulmalıdır.

--- main.py
+++ main.py
@@ -9,22 +9,19 @@ ws = wb.active
 
 # Aktif çalışma sayfasının adını yazdırma
 print(wb.sheetnames)
-for x in range(2, 90):
-  print(ws.cell(x,2).value)
+for x in range(2, 3):
+  adiSoyadi = ws.cell(x,2).value
+  os.chdir(sys.path[0])
+  doc = DocxTemplate("Template.docx")
+  an = tarih.datetime.now()
+  context = {
+      "adsoyad": adiSoyadi,
+      "baslik": "Bilmem ne Bildiri Adi",
+      "universite": "Batman Üniversitesi",
+      "tarih": tarih.datetime.strftime(an, '%d %B %Y')
+  }
 
-#
-# os.chdir(sys.path[0])
-# doc = DocxTemplate("Template.docx")
-# adiSoyadi = "Ammar ASLAN"
-# an = tarih.datetime.now()
-# context = {
-#     "adsoyad": adiSoyadi,
-#     "baslik": "Bilmem ne Bildiri Adi",
-#     "universite": "Batman Üniversitesi",
-#     "tarih": tarih.datetime.strftime(an, '%d %B %Y')
-# }
-#
-# doc.render(context)
-# dokumanAdi = adiSoyadi + ".docx";
-# doc.save(dokumanAdi)
-# convert(dokumanAdi, "output.pdf")
+  doc.render(context)
+  dokumanAdi = adiSoyadi + ".docx";
+  doc.save(dokumanAdi)
+  convert(dokumanAdi, adiSoyadi+".pdf")
